edf2f9b856f2adb87f0abdde20b5773e30429774
Cruz Diego <diegohiller754@gmail.com>
2025-11-14 16:27:50 -0300

    Configuracion e implementacion de lo solicitado en las consignas

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,40 @@
 import { Link } from "react-router";
+import { useForm } from "../hooks/useForm";
+import { useNavigate } from "react-router";
+
 
 export const LoginPage = () => {
-  // TODO: Integrar lógica de autenticación aquí
   // TODO: Implementar useForm para el manejo del formulario
+  const navigate = useNavigate();
+  const { formState, handleChange, handleReset } = useForm({
+    username: "",
+    password: "",
+  });
+  // TODO: Integrar lógica de autenticación aquí
+
   // TODO: Implementar función handleSubmit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:3000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(formState),
+      });
+
+      if (res.ok) {
+        navigate("/home");
+      } else {
+        alert("Credenciales incorrectas");
+        handleReset();
+      }
+    } catch (e) {
+      console.error("Error: ", e.message);
+    }
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4 py-8">
